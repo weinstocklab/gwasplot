ef07e4015ac9077c3fdaef76b0ccd641a9bb5e08
Josh Weinstock <joshua.s.weinstock@gmail.com>
2025-09-28 15:02:26 -0400

    adding ATM to chip gene list

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -31,7 +31,7 @@ ideogram = arrow::read_tsv_arrow("centromere.txt") %>%
 
 usethis::use_data(ideogram, overwrite = TRUE)
 
-chip_genes = c("ASXL1", "ASXL2", "BCOR", "BCORL1", "BRAF", "BRCC3", "CBL", "CBLB", 
+chip_genes = c("ASXL1", "ASXL2", "ATM", "BCOR", "BCORL1", "BRAF", "BRCC3", "CBL", "CBLB", 
                  "CEBPA", "CREBBP", "CSF1R", "CSF3R", "CTCF", "CUX1", "DNMT3A", "EED", 
                  "EP300", "ETNK1", "ETV6", "EZH2", "FLT3", "GATA1", "GATA2", "GATA3", 
                  "GNA13", "GNAS", "GNB1", "IDH1", "IDH2", "IKZF1", "IKZF2", "IKZF3", 
